d6531bbd1ffcdd3d6823c9c9cebd901ab5cbdeba
Kaka <141387279+Yakginkaomangai@users.noreply.github.com>
2025-10-30 14:22:33 +0700

    admin navbar

--- track-me/components/NavWrapper.tsx
+++ track-me/components/NavWrapper.tsx
@@ -2,13 +2,17 @@
 
 import { usePathname } from "next/navigation";
 import HomeNav from "./HomeNav";
-import SignInNav from "./SignInNav"
+import SignInNav from "./SignInNav";
+import AdminNav from "./AdminNav";
 
 export default function NavWrapper() {
     const pathname = usePathname();
 
     if (pathname === "/signin") return <SignInNav/>
     else if (pathname === "/signup") return <SignInNav/>
+
+    if (pathname === "/admin/dashboard") return <AdminNav/>
+    else if (pathname === "/admin/user") return <AdminNav/>
 
     return <HomeNav/>
 }
