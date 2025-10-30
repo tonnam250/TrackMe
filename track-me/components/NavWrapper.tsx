52531be179567ea8d97a366cd6473754a493a78b
Kaka <141387279+Yakginkaomangai@users.noreply.github.com>
2025-10-31 00:56:55 +0700

    fix admin nav

--- track-me/components/NavWrapper.tsx
+++ track-me/components/NavWrapper.tsx
@@ -1,18 +1,19 @@
-"use client"
-
+"use client";
 import { usePathname } from "next/navigation";
 import HomeNav from "./HomeNav";
 import SignInNav from "./SignInNav";
 import AdminNav from "./AdminNav";
 
 export default function NavWrapper() {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    if (pathname === "/signin") return <SignInNav/>
-    else if (pathname === "/signup") return <SignInNav/>
+  if (pathname === "/signin" || pathname === "/signup") {
+    return <SignInNav />;
+  }
 
-    if (pathname === "/admin/dashboard") return <AdminNav/>
-    else if (pathname === "/admin/user") return <AdminNav/>
+  if (pathname.startsWith("/admin")) {
+    return <AdminNav />;
+  }
 
-    return <HomeNav/>
+  return <HomeNav />;
 }
